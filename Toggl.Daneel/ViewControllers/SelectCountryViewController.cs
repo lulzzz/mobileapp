71a8e284e34027af2832bd7cc0573d3ee4b0993d
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2018-05-02 20:15:43 +0300

    ✨Add SelectCountryVC

---
+++ Toggl.Daneel/ViewControllers/SelectCountryViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Toggl.Daneel.ViewControllers
+{
+    public partial class SelectCountryViewController : UIViewController
+    {
+        public SelectCountryViewController() : base("SelectCountryViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
