0c9d912d36542adb01e840b3f5a91e95a5fc6a5e
Ricardo Sánchez Sotres <odrakir@gmail.com>
2018-05-03 12:42:09 +0200

    :sparkles: Add interactors to get elements that failed to sync

---
+++ Toggl.Foundation/Interactors/Client/GetClientsThatFailedToSyncInteractor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+using Toggl.Multivac;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Interactors.Client
+{
+    public class GetClientsThatFailedToSync : IInteractor<IObservable<IEnumerable<IDatabaseClient>>>
+    {
+        private readonly ITogglDatabase database;
+
+        public GetClientsThatFailedToSync(ITogglDatabase database)
+        {
+            Ensure.Argument.IsNotNull(database, nameof(database));
+
+            this.database = database;
+        }
+
+        public IObservable<IEnumerable<IDatabaseClient>> Execute()
+            => database.Clients
+                .GetAll()
+                .Select(failedToSync);
+
+        private IEnumerable<IDatabaseClient> failedToSync(IEnumerable<IDatabaseClient> clients)
+        {
+            return clients.Where(c => c.SyncStatus == SyncStatus.SyncFailed);
+        }
+    }
+}
---
+++ Toggl.Foundation/Interactors/Project/GetProjectsThatFailedToSyncInteractor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+using Toggl.Multivac;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Interactors.Project
+{
+    public class GetProjectsThatFailedToSyncInteractor : IInteractor<IObservable<IEnumerable<IDatabaseProject>>>
+    {
+        private readonly ITogglDatabase database;
+
+        public GetProjectsThatFailedToSyncInteractor(ITogglDatabase database)
+        {
+            Ensure.Argument.IsNotNull(database, nameof(database));
+
+            this.database = database;
+        }
+
+        public IObservable<IEnumerable<IDatabaseProject>> Execute()
+            => database.Projects
+                .GetAll()
+                .Select(failedToSync);
+
+        private IEnumerable<IDatabaseProject> failedToSync(IEnumerable<IDatabaseProject> projects)
+        {
+            return projects.Where(p => p.SyncStatus == SyncStatus.SyncFailed);
+        }
+    }
+}
