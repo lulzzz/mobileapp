4517346bdb548248394f32d93da1057fdb6123e9
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2018-05-04 18:43:28 +0300

    ✨Add CountrySelection UI
    Connects everything to the ViewModel

--- Toggl.Daneel/ViewControllers/SelectCountryViewController.cs
+++ Toggl.Daneel/ViewControllers/SelectCountryViewController.cs
@@ -1,25 +1,52 @@
-﻿using System;
-
+﻿using MvvmCross.Binding.BindingContext;
+using Toggl.Daneel.ViewSources;
+using Toggl.Foundation.MvvmCross.Helper;
+using Toggl.Foundation.MvvmCross.ViewModels;
 using UIKit;
 
 namespace Toggl.Daneel.ViewControllers
 {
-    public partial class SelectCountryViewController : UIViewController
+    public partial class SelectCountryViewController : KeyboardAwareViewController<SelectCountryViewModel>
     {
-        public SelectCountryViewController() : base("SelectCountryViewController", null)
+        public SelectCountryViewController() : base(nameof(SelectCountryViewController))
         {
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            var source = new CountryTableViewSource(CountriesTableView);
+            CountriesTableView.Source = source;
+
+            var bindingSet = this.CreateBindingSet<SelectCountryViewController, SelectCountryViewModel>();
+
+            bindingSet.Bind(source).To(vm => vm.Suggestions);
+            bindingSet.Bind(SearchTextField).To(vm => vm.Text);
+            bindingSet.Bind(CloseButton).To(vm => vm.CloseCommand);
+            bindingSet.Bind(source)
+                      .For(v => v.SelectionChangedCommand)
+                      .To(vm => vm.SelectCountryCommand);
+
+            bindingSet.Bind(source)
+                      .For(v => v.Text)
+                      .To(vm => vm.Text);
+
+            bindingSet.Apply();
+
+            SearchTextField.BecomeFirstResponder();
         }
 
-        public override void DidReceiveMemoryWarning()
+        protected override void KeyboardWillShow(object sender, UIKeyboardEventArgs e)
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            BottomConstraint.Constant = e.FrameEnd.Height;
+            UIView.Animate(Animation.Timings.EnterTiming, () => View.LayoutIfNeeded());
+        }
+
+        protected override void KeyboardWillHide(object sender, UIKeyboardEventArgs e)
+        {
+            BottomConstraint.Constant = 0;
+            UIView.Animate(Animation.Timings.EnterTiming, () => View.LayoutIfNeeded());
         }
     }
 }
---
+++ Toggl.Daneel/Views/CountrySelection/CountryViewCell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using UIKit;
+
+namespace Toggl.Daneel.Views.CountrySelection
+{
+    public partial class CountryViewCell : MvxTableViewCell
+    {
+        public static readonly NSString Key = new NSString(nameof(CountryViewCell));
+        public static readonly UINib Nib;
+
+        static CountryViewCell()
+        {
+            Nib = UINib.FromName(nameof(CountryViewCell), NSBundle.MainBundle);
+        }
+
+        protected CountryViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            this.DelayBind(() =>
+            {
+                var bindingSet = this.CreateBindingSet<CountryViewCell, string>();
+
+                bindingSet.Bind(NameLabel).To(vm => vm);
+
+                bindingSet.Apply();
+            });
+        }
+    }
+}
