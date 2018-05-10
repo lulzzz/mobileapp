afdd25dbdc47af09dae6728837042897fe50daee
Simon Rozsival <simon@rozsival.com>
2018-05-10 18:05:14 +0200

    :sparkles: Add push single state

---
+++ Toggl.Foundation/Sync/States/Push/PushSingleState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reactive.Linq;
+using Toggl.Foundation.DataSources.Interfaces;
+using Toggl.Foundation.Models.Interfaces;
+using Toggl.Multivac;
+using Toggl.PrimeRadiant;
+
+namespace Toggl.Foundation.Sync.States.Push
+{
+    internal sealed class PushSingleState<TDatabaseModel, TThreadsafeModel>
+        where TDatabaseModel : IDatabaseSyncable
+        where TThreadsafeModel : class, TDatabaseModel, IThreadsafeModel
+    {
+        private readonly ISingleObjectDataSource<TThreadsafeModel, TDatabaseModel> dataSource;
+
+        public StateResult<TThreadsafeModel> PushEntity { get; } = new StateResult<TThreadsafeModel>();
+
+        public StateResult NothingToPush { get; } = new StateResult();
+
+        public PushSingleState(ISingleObjectDataSource<TThreadsafeModel, TDatabaseModel> dataSource)
+        {
+            Ensure.Argument.IsNotNull(dataSource, nameof(dataSource));
+        
+            this.dataSource = dataSource;
+        }
+
+        public IObservable<ITransition> Start() =>
+            dataSource
+                .Get()
+                .Where(entity => entity.SyncStatus == SyncStatus.SyncNeeded)
+                .SingleOrDefaultAsync()
+                .Select(entity =>
+                    entity != null
+                        ? (ITransition)PushEntity.Transition(entity)
+                        : NothingToPush.Transition());
+    }
+}
