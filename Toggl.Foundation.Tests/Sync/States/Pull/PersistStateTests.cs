49bc5bc37a137d8e4b7840766d31a03713b4e7c1
Simon Rozsival <simon@rozsival.com>
2018-05-11 14:03:11 +0200

    :lipstick: Rename test method

--- Toggl.Foundation.Tests/Sync/States/Pull/PersistStateTests.cs
+++ Toggl.Foundation.Tests/Sync/States/Pull/PersistStateTests.cs
@@ -180,7 +180,7 @@ namespace Toggl.Foundation.Tests.Sync.States
 
         [Theory, LogIfTooSlow]
         [MemberData(nameof(ApiExceptions.ExceptionsWhichCauseRethrow), MemberType = typeof(ApiExceptions))]
-        public void ThrowsWhenCertainExceptionsAreCaught(Exception exception)
+        public void ThrowsWhenExceptionsWhichShouldCauseRetrhrowAreCaught(Exception exception)
         {
             var state = new PersistState<ITestModel, IDatabaseTestModel, IThreadsafeTestModel>(dataSource, sinceParameterRepository, TestModel.Clean);
             Exception caughtException = null;
